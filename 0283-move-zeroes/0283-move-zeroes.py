67b1a128afaf196e4d81f89f1cd5d8f720a04961
Ethan Smith <83934029+SirSmitty@users.noreply.github.com>
2022-10-13 10:29:05 -0700

    Time: 2536 ms (6.36%), Space: 14.6 MB (82.66%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        ans = []
+        zeros = []
+        for index,num in enumerate(nums):
+            if(num == 0):
+                nums.append(nums[index])
+                nums.remove(nums[index])
+        
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
